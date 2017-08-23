23345d4d0c8c94e1e308cd80587d35ed91fc515d
nikai <keni1006@163.com>
2017-08-23 22:02:51 +0800

    init

---
+++ src/main/java/org/apache/zookeeper/test/ClientBase.java
@@ -0,0 +1,14 @@
+package org.apache.zookeeper.test;
+
+import java.io.File;
+
+public class ClientBase
+{
+
+	public static File createTmpDir()
+	{
+		// TODO Auto-generated method stub
+		return new File("C:\\server\\zookeeper1\\zookeeper-3.4.6\\conf");
+	}
+
+}
